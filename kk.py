2a39d670d2c886e88d739dd4a85de82ca2a5ae57
mark taylor <gb051528@li-d9a658cc-20ae-11b2-a85c-a8883b828306.ibm.com>
2020-08-25 14:45:06 +0100

    newprog

--- kk.py
+++ kk.py
@@ -1,5 +1,9 @@
 from kafka import KafkaProducer
-producer = KafkaProducer(bootstrap_servers='bae-es-kafka-bootstrap.mark-nr.svc:9092')
-producer.send('temperature', b'Hello, World!')
-producer.send('temperature', key=b'message-two', value=b'This is Kafka-Python')
+
+if __name__ == "__main__":
+    while true:
+        producer = KafkaProducer(bootstrap_servers='bae-es-kafka-bootstrap.mark-nr.svc:9092')
+        producer.send('sensor-readings', b'Hello, World!')
+        producer.send('sensor-readings', key=b'message-two', value=b'This is Kafka-Python')
+        sleep(30)
 
